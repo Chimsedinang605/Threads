e9f2a445b43cbac42174abee3059b89bd5735240
chimsedinang <nguyenthihong24062005@gmail.com>
2025-03-19 18:52:33 +0700

    chon img truoc khi dang ki

---
+++ app/src/main/java/com/example/threads/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.example.threads.model
+
+data class UserModel(
+   val name: String = "",
+   val username: String = "",
+   val email: String = "",
+   val password: String = "",
+   val toString: String = "",
+   val uid: String = ""
+)
